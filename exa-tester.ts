4d5e072f79eef635c6ba86f80c894c3a79b3a036
Yaseen Shaikh <yaseen.nvt@gmail.com>
2019-06-06 20:02:19 -0400

    Small Changes to Tester

--- exa-tester.ts
+++ exa-tester.ts
@@ -4,11 +4,12 @@ import * as util from "util";
 
 
 class ExaTester {
-    constructor() {
-
-    }
-
     static verifyState(exa: EXA, testState: EXAState): boolean {
         return util.isDeepStrictEqual(exa.captureState(), testState);
+    }
+
+    static verifyRegisterContent(exa: EXA, testState: EXAState) {
+        return exa.X === testState.X
+            && exa.T === testState.T;
     }
 }
